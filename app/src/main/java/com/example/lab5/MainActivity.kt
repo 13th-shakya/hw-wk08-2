b6d4c1766c442d51c27498fae7b97f45eb35aa37
Riley Ho <riley@rileychh.dev>
2024-11-02 17:29:42 +0800

    chore: optimize imports

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.lab5
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.viewpager2.widget.ViewPager2
 import com.example.lab5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
